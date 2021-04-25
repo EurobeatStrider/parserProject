640eb4f45fb39ef545d84a3be43bacf4dc60eba2
polo <yxngboypolo@gmail.com>
2021-04-25 16:12:08 -0500

    removed line 7

--- src/scanner.java
+++ src/scanner.java
@@ -4,7 +4,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.*;
 
-//ocmmit
 public class scanner
 {
     public static void main(String args[]) throws IOException
